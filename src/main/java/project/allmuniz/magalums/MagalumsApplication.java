3746ec19e5e4a2787542ef9fd235ecf71ce21589
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2024-10-01 08:59:06 -0300

    Initial commit

---
+++ src/main/java/project/allmuniz/magalums/MagalumsApplication.java
@@ -0,0 +1,13 @@
+package project.allmuniz.magalums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagalumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagalumsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/allmuniz/magalums/MagalumsApplicationTests.java
@@ -0,0 +1,13 @@
+package project.allmuniz.magalums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagalumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
