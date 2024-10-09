2cf5f573aa3079493878e0e8ae762e7e8d899a8e
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2024-10-09 17:13:29 -0300

    Update Swagger

--- src/main/java/project/allmuniz/magalums/MagalumsApplication.java
+++ src/main/java/project/allmuniz/magalums/MagalumsApplication.java
@@ -1,11 +1,20 @@
 package project.allmuniz.magalums;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Magalu",
+				description = "Desafio Back-End Magalu",
+				version = "1"
+		)
+)
 public class MagalumsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/project/allmuniz/magalums/controller/NotificationController.java
+++ src/main/java/project/allmuniz/magalums/controller/NotificationController.java
@@ -1,5 +1,7 @@
 package project.allmuniz.magalums.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.allmuniz.magalums.controller.dto.ScheduleNotificationDto;
@@ -8,6 +10,7 @@ import project.allmuniz.magalums.service.NotificationService;
 
 @RestController
 @RequestMapping("/notifications")
+@Tag(name = "Notification", description = "Notification Manager")
 public class NotificationController {
 
     private final NotificationService notificationService;
@@ -17,18 +20,24 @@ public class NotificationController {
     }
 
     @PostMapping
+    @Operation(description = "Scheduling",
+            summary = "Notification Scheduling")
     public ResponseEntity<Void> scheduleNotification(@RequestBody ScheduleNotificationDto dto) {
         notificationService.scheduleNotification(dto);
         return ResponseEntity.accepted().build();
     }
 
     @GetMapping("/{notificationId}")
+    @Operation(description = "Search",
+            summary = "Search for a notification")
     public ResponseEntity<Notification> getNotification(@PathVariable("notificationId") long notificationId) {
         var notification = notificationService.findById(notificationId);
         return notification.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @DeleteMapping("/{notificationId}")
+    @Operation(description = "Canceling",
+            summary = "Canceling a notification")
     public ResponseEntity<Void> cancelNotification(@PathVariable("notificationId") long notificationId) {
         notificationService.cancelNotification(notificationId);
         return ResponseEntity.noContent().build();
